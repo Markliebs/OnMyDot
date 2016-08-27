ccb626d7315c1400be03400a2fd47c6564b6beba
markliebs <markliebs@me.com>
2016-08-27 12:57:58 -0400

    slight changes and added procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,91 +1,115 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@section('title') On My Dot @endsection @extends('layout') @section('content')
+<div class="container">
+    <div class="jumbotron">
 
-        <title>Laravel</title>
+        <div class="boxSize" id="boxSize">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+            <div class="h1">I'm on my dot</h1><br>
+            </div>
+            <div class="row">
+                <form class="form-horizontal">
+                    <fieldset>
+                        <div class="form-group">
+                            <label for="select" class="control-label"><h5>Side</h5></label>
+                            <div class="selWidth">
+                                <select class="form-control" id="selectSide selWidth">
+                                        <option value="one">1</option>
+                                        <option value="two">2</option>
+                                        </select>
+                                <br>
+                            </div>
+                        </div>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway';
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+                        <div class="form-group">
+                            <label for="select" class="control-label"><h5>Yard Line</h5></label>
+                            <div class="selWidth">
+                                <select class="form-control" id="selectLine selWidth">
+                    <option value="goal">0</option>
+                    <option value="five">5</option>
+                    <option value="ten">10</option>
+                    <option value="fifteen">15</option>
+                    <option value="twenty">20</option>
+                    <option value="twentyFive">25</option>
+                    <option value="thirty">30</option>
+                    <option value="thirtyFive">35</option>
+                    <option value="forty">40</option>
+                    <option value="fortyFive">45</option>
+                    <option value="fifty">50</option>
+                    </select>
+                                <br>
 
-            .full-height {
-                height: 100vh;
-            }
+                            </div>
+                        </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    <a href="{{ url('/login') }}">Login</a>
-                    <a href="{{ url('/register') }}">Register</a>
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                        <div class="form-group">
+                            <label for="select" class="control-label"><h5>Distance From Sideline</h5></label>
+                            <div class="selWidth">
+                                <select class="form-control" id="distanceFromSideline">
+                    <option value="one">1</option>
+                    <option value="two">2</option>
+                    <option value="three">3</option>
+                    <option value="four">4</option>
+                    <option value="five">5</option>
+                    <option value="six">6</option>
+                    <option value="seven">7</option>
+                    <option value="eight">8</option>
+                    <option value="nine">9</option>
+                    <option value="ten">10</option>
+                    <option value="eleven">11</option>
+                    <option value="twelve">12</option>
+                    <option value="thirteen">13</option>
+                    <option value="fourteen">14</option>
+                    <option value="fifteen">15</option>
+                    <option value="sixteen">16</option>
+                    <option value="seventeen">17</option>
+                    <option value="eighteen">18</option>
+                    <option value="nineteen">19</option>
+                    <option value="twenty">20</option>
+                    <option value="twentyOne">21</option>
+                    <option value="twentyTwo">22</option>
+                    <option value="twentyThree">23</option>
+                    <option value="twentyFour">24</option>
+                    <option value="twentyFive">25</option>
+                    <option value="twentySix">26</option>
+                    <option value="twentySeven">27</option>
+                    <option value="twentyEight">28</option>
+                        </select>
+                                <br>
+                            </div>
+                        </div>
             </div>
         </div>
-    </body>
-</html>
+
+        {{-- <div class="row">
+
+
+            <div class="form-group">
+                <label for="select" class="control-label"><h5>Hash Marks</h5></label>
+                <div class="selWidth">
+                    <select class="form-control" id="selectHash">
+                                        <option value="HS">High School</option>
+                                        <option value="NCAA">College</option>
+                                        <option value="NFL">Professional</option>
+                                        </select>
+                    <br>
+                </div>
+            </div>
+
+        </div> --}}
+
+
+
+
+        <div class="form-group">
+
+            <div class="col-lg-10 col-lg-offset-2">
+                <button type="reset" class="btn btn-danger">Cancel</button>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </div>
+        </div>
+
+        </fieldset>
+        </form>
+    </div>
+</div>
+@stop
