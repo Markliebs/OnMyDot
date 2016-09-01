f24ecec97af23ca0901f7582665822ad48a6ab7b
markliebs <markliebs@me.com>
2016-08-31 20:45:32 -0400

    Cleaning up code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,45 +6,43 @@
 
     <div class="row">
         <div class="grid">
-        <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
-            <img src="http://placehold.it/750x350/007300/000000?text=Football Field Grid Goes Here">
-        </div>
-        <div class="setList">
-        <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
-            <img src="http://placehold.it/350x350/ffffff/000000?text=Set List Goes Here">
-        </div>
+            <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-xl-8">
+                <img src="http://placehold.it/750x350/007300/000000?text=Football Field Grid Goes Here">
+            </div>
+            <div class="setList">
+                <div class="col-xs-12 col-sm-12 col-md-3 col-lg-3 col-xl-3">
+                    <img src="http://placehold.it/350x350/ffffff/000000?text=Set List Goes Here">
+                </div>
+            </div>
         </div>
     </div>
-    </div>
 
-
-
-<div class="stuffandthings">
-    <div class="panel panel-success">
-        <div class="panel-heading">
-            <h3 class="panel-title">Enter a new set</h3>
-        </div>
-        <div class="panel-body">
-            <div class="row">
-                <form class="form-horizontal">
-                    <fieldset>
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Field Side</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="selectSide">
+    <div class="stuffandthings">
+        <div class="panel panel-success">
+            <div class="panel-heading">
+                <h3 class="panel-title">Enter a new set</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <form class="form-horizontal">
+                        <fieldset>
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Field Side</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="selectSide">
                                         <option value="one">1</option>
                                         <option value="two">2</option>
                                         </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Yard Line</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="selectLine">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Yard Line</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="selectLine">
                                     <option value="goal">0</option>
                                     <option value="five">5</option>
                                     <option value="ten">10</option>
@@ -57,69 +55,68 @@
                                     <option value="fortyFive">45</option>
                                     <option value="fifty">50</option>
                                 </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>In or Out</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="selectIn">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>In or Out</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="selectIn">
                                     <option value="inside">Inside</option>
                                     <option value="outside">Outside</option>
                                 </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Steps Off</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="distanceFromYardline">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Steps Off</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="distanceFromYardline">
                                             <option value="one">1</option>
                                             <option value="two">2</option>
                                             <option value="three">3</option>
                                             <option value="four">4</option>
                                         </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Hash Mark</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="hashMark">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Hash Mark</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="hashMark">
                                             <option value="front">Front</option>
                                             <option value="back">Back</option>                                        </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Front-Back</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="fOrb">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Front-Back</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="fOrb">
                                             <option value="front">Front</option>
                                             <option value="back">Back</option>                                        </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
-                            <div class="form-group">
-                                <label for="select" class="control-label"><h5>Steps from</h5></label>
-                                <div class="selWidth">
-                                    <select class="form-control" id="stepsFromHash">
+                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1">
+                                <div class="form-group">
+                                    <label for="select" class="control-label"><h5>Steps from</h5></label>
+                                    <div class="selWidth">
+                                        <select class="form-control" id="stepsFromHash">
                                             <option value="one">1</option>
                                             <option value="two">2</option>
                                             <option value="three">3</option>
@@ -149,26 +146,23 @@
                                             <option value="twentySeven">27</option>
                                             <option value="twentyEight">28</option>
                                         </select>
-                                    <br>
+                                        <br>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="form-group clickyThings">
-                            <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
-                                <button type="submit" class="btn btn-success btn-sm submitButton" id="submitButton">Submit Dot</button>
-                                <button type="clear" class="btn btn-danger btn-sm clearButton" id="clearButton">Clear  Form</button>
+                            <div class="form-group clickyThings">
+                                <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
+                                    <button type="submit" class="btn btn-success btn-sm submitButton" id="submitButton">Submit Dot</button>
+                                    <button type="clear" class="btn btn-danger btn-sm clearButton" id="clearButton">Clear  Form</button>
+                                </div>
                             </div>
-                        </div>
+                </div>
+                </fieldset>
+                </form>
             </div>
-            </fieldset>
-            </form>
         </div>
     </div>
-</div>
 
 
 
-    @stop 
-    
-    
-    {{-- col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1 --}}
+    @stop
