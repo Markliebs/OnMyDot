6b2157408447561b5fb4c8baf17c2768ddf5301d
markliebs <markliebs@me.com>
2016-08-31 21:07:36 -0400

    Buttons

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -155,8 +155,9 @@
                             </div>
                             <div class="form-group clickyThings">
                                 <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 col-xl-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 col-xl-offset-1">
-                                    <button type="submit" class="btn btn-success btn-sm submitButton" id="submitButton">Submit Dot</button>
-                                    <button type="clear" class="btn btn-danger btn-sm clearButton" id="clearButton">Clear  Form</button>
+                                    <button type="submit" class="btn btn-success btn-sm submitButton" id="submitButton">Send to DotBook</button>
+                                    <button type="clear" class="btn btn-danger btn-sm clearButton" id="clearButton">Clear the Data</button>
+                                    <button type="dots" class="btn btn-primary btn-sm dotButton" id="dotButton">Go to DotBook</button>
                                 </div>
                             </div>
                 </div>
