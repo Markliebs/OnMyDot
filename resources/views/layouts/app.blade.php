0e45d7318f5334cf552d893fdbe117d4129ec645
Lieberman <markliebs@me.com>
2016-09-06 09:15:20 -0400

    Changed link in navbar to go to correct place

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
                 </button>
 
                 <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     On My Dot
                 </a>
             </div>
