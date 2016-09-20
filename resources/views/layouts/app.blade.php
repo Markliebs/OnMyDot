4036d5f289ae7bdcb15e514ec9afc29932213205
markliebs <markliebs@me.com>
2016-09-20 12:26:53 -0400

    Ok Last one I promise.  Added nav menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,100 +2,110 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>On My Dot</title>
+	<title>On My Dot</title>
 
-    <!-- Styles -->
-    <link rel="stylesheet" href="/css/reset.css" type="text/css">
-    <link rel="stylesheet" href="/css/style.css" type="text/css">
-    <link href="/css/app.css" rel="stylesheet">
-    <link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
-    <link rel="stylesheet" type="text/css" href="/css/sweetalert.css">
+	<!-- Styles -->
+	<link rel="stylesheet" href="/css/reset.css" type="text/css">
+	<link rel="stylesheet" href="/css/style.css" type="text/css">
+	<link href="/css/app.css" rel="stylesheet">
+	<link rel="stylesheet" href="/css/bootstrap.css" type="text/css">
+	<link rel="stylesheet" type="text/css" href="/css/sweetalert.css">
 
-    <!-- Scripts -->
+	<!-- Scripts -->
 
-    <script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
-    <script src="/js/click.js"></script>
-    <script src="/js/sweetalert.js"></script>
+	<script src="https://code.jquery.com/jquery-3.1.0.min.js" integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s=" crossorigin="anonymous"></script>
+	<script src="/js/click.js"></script>
+	<script src="/js/sweetalert.js"></script>
 
-    <script>
-        window.Laravel = <?php echo json_encode([
+	<script>
+		window.Laravel = <?php echo json_encode([
             'csrfToken' => csrf_token(),
         ]); ?>
-    </script>
+	</script>
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
+	<nav class="navbar navbar-inverse navbar-static-top">
+		<div class="container">
+			<div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+				<!-- Collapsed Hamburger -->
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+					<span class="sr-only">Toggle Navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
 
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/home') }}">
+				<!-- Branding Image -->
+				<a class="navbar-brand" href="{{ url('/home') }}">
                     On My Dot
                 </a>
-            </div>
 
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    &nbsp;
-                </ul>
+			</div>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+			<div class="collapse navbar-collapse" id="app-navbar-collapse">
+				<!-- Left Side Of Navbar -->
+				<ul class="nav navbar-nav">
+					&nbsp;
+				</ul>
+
+				<!-- Right Side Of Navbar -->
+				<ul class="nav navbar-nav navbar-right">
+
+				<!--Navigation menu-->
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Go To... <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="/dotbook">Dot Book</a></li>
+							<li><a href="/home">Enter A New Dot</a></li>
+						</ul>
+					</li>
+
+					<!-- Authentication Links -->
+					@if (Auth::guest())
+					<li><a href="{{ url('/login') }}">Login</a></li>
+					<li><a href="{{ url('/register') }}">Register</a></li>
+					@else
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
                             </a>
 
-                        <ul class="dropdown-menu" role="menu">
-                            <br>
-                            <li><a href="/home">Enter A New Dot</a></li>
-                            <li><a href="/dotbook">Dot Book</a></li>
-                            <hr>
-                            <li>
-                                <a href="{{ url('/logout') }}" onclick="event.preventDefault();
+						<ul class="dropdown-menu" role="menu">
+							<br>
+							<li><a href="/home">Enter A New Dot</a></li>
+							<li><a href="/dotbook">Dot Book</a></li>
+							<hr>
+							<li>
+								<a href="{{ url('/logout') }}" onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
                                         Logout
                                     </a>
 
+								<form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+									{{ csrf_field() }}
+								</form>
+							</li>
+						</ul>
+					</li>
+					@endif
+				</ul>
+			</div>
+		</div>
+	</nav>
 
-                                <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                                    {{ csrf_field() }}
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+	@yield('content')
 
-    @yield('content')
-
-    <!-- Scripts -->
-    <script src="/js/app.js"></script>
+	<!-- Scripts -->
+	<script src="/js/app.js"></script>
 </body>
 
 </html>
