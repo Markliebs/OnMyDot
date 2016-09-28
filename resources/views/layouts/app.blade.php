fc6bb61ce117324d68b8507825814ccb13fdafe5
markliebs <markliebs@me.com>
2016-09-28 15:20:10 -0400

    Added Title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<title>Welcome to OnMyDot.Com</title
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
